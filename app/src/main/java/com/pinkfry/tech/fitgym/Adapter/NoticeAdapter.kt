c247bf2eb6f17a88277a43f3dd366c914c635832
boffin311 <himanshu31198@gmail.com>
2020-04-05 20:43:46 +0530

    The login page is made

---
+++ app/src/main/java/com/pinkfry/tech/fitgym/Adapter/NoticeAdapter.kt
@@ -0,0 +1,31 @@
+package com.pinkfry.tech.fitgym.Adapter
+
+import android.app.Activity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.pinkfry.tech.fitgym.R
+import kotlinx.android.synthetic.main.adatper_notice.view.*
+
+class NoticeAdapter(private var arrayList:ArrayList<String>):RecyclerView.Adapter<NoticeAdapter.MyHolder>(){
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder {
+        var li=parent.context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        var view=li.inflate(R.layout.adatper_notice,parent,false)
+        return  MyHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return arrayList.size
+    }
+
+    override fun onBindViewHolder(holder: MyHolder, position: Int) {
+       holder.tvSingleNotice.text= "$position ${arrayList[position]}"
+    }
+
+    class MyHolder(itemview:View) : RecyclerView.ViewHolder(itemview){
+      var tvSingleNotice=itemview.tvSingleNotice;
+    }
+}
