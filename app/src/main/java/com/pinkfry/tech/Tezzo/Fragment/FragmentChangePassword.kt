e516c40366e19f8dae86aeccf71deba799d5fab8
boffin311 <himanshu31198@gmail.com>
2020-04-21 18:28:19 +0530

    All basic feature are added and in this commit only some UI changes are made

--- app/src/main/java/com/pinkfry/tech/Tezzo/Fragment/FragmentChangePassword.kt
+++ app/src/main/java/com/pinkfry/tech/Tezzo/Fragment/FragmentChangePassword.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.pinkfry.tech.Tezzo.Activity.MainActivity
 import com.pinkfry.tech.Tezzo.R
@@ -18,25 +19,29 @@ class FragmentChangePassword : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view=inflater.inflate(R.layout.fragment_change_password,container,false)
+        val view = inflater.inflate(R.layout.fragment_change_password, container, false)
         view.btnChangePassword.setOnClickListener {
-            val changedPassword=view.etEnterPassword.text.toString()
-            val confirmPassword=view.etConfirmPassword.text.toString()
-            if(changedPassword.isEmpty()){
+            val changedPassword = view.etEnterPassword.text.toString()
+            val confirmPassword = view.etConfirmPassword.text.toString()
+            if (changedPassword.isEmpty()) {
                 view.etEnterPassword.error = "This field can't be empty"
-            }else if(confirmPassword.isEmpty()){
-               view.etConfirmPassword.error="This field can't be empty"
-            }
-            else if (changedPassword != confirmPassword){
-               view.tvPasswordNotMatch.visibility=View.VISIBLE
-            }
-            else{
+            } else if (confirmPassword.isEmpty()) {
+                view.etConfirmPassword.error = "This field can't be empty"
+            } else if (changedPassword != confirmPassword) {
+                view.tvPasswordNotMatch.visibility = View.VISIBLE
+            } else {
+                view.tvPasswordNotMatch.visibility = View.INVISIBLE
+                Toast.makeText(
+                    context,
+                    "This functionality will be added soon...",
+                    Toast.LENGTH_SHORT
+                ).show()
                 //Request marni hai server pe
 
             }
         }
         view.tvSkip.setOnClickListener {
-        var intent=Intent(requireActivity(),MainActivity::class.java)
+            var intent = Intent(requireActivity(), MainActivity::class.java)
             startActivity(intent)
         }
         return view
